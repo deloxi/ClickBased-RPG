fa872d9a5e6c1d4e4f351e15933b4deb2d27f3d4
Deloxi <lukas.munther@gmail.com>
2019-11-23 01:02:01 +0100

    initial commit

---
+++ Basestats.js
@@ -0,0 +1,39 @@
+// if characterLevel > 1
+  //  statIds = statIds + [1, 1, 1, 2, 10, 5];
+//^to be fixed^
+
+
+var person = {
+  name: "Lukas",
+  age: 20
+}
+
+console.log("My name is " + JSON.stringify(person))
+console.log(`My name is ${JSON.stringify(person)}`)
+
+let characterLevel = 5;
+let startStatIds = [10, 10, 10, 20, 100, 50];
+let [Str, Agi, Int, Sta, HP, MP] = startStatIds;
+
+
+function getCharacterStats(level, stats) {
+  //level 1 = 1
+  //level 2 = 1,1
+  //level 3 = 1,2
+  let progress = (level-1)*0.1+1;
+  return Math.round(stats*progress);
+}
+
+function showCharacterStats() {
+  console.log("Character level = " + characterLevel)
+  console.log(`Character level = ${characterLevel}`);
+  console.log("Character level" + " = " + characterLevel);
+  console.log("Str" + " " + getCharacterStats(characterLevel, Str))
+  console.log("Agi" + " " + getCharacterStats(characterLevel, Agi))
+  console.log("Int" + " " + getCharacterStats(characterLevel, Int))
+  console.log("Sta" + " " + getCharacterStats(characterLevel, Sta))
+  console.log("HP" + " " + getCharacterStats(characterLevel, HP))
+  console.log("MP" + " " + getCharacterStats(characterLevel, MP))
+}
+
+showCharacterStats();
